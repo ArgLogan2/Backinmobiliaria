7c409ef5777d8c40efa6f06f4697a79bd4570853
JJROMA <jj_roma@yahoo.com.ar>
2022-06-06 21:00:39 -0300

    john 001

--- src/main/java/com/proinmob/inmobiliaria/model/Usuarios.java
+++ src/main/java/com/proinmob/inmobiliaria/model/Usuarios.java
@@ -2,5 +2,21 @@
 package com.proinmob.inmobiliaria.model;
 
 public class Usuarios {
+    private Long id;
+    private String Nombre;
+    private String Apellido;
+    private String email;
+    private boolean admin;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(Long id, String Nombre, String Apellido, String email, boolean admin) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.email = email;
+        this.admin = admin;
+    }
     
 }
