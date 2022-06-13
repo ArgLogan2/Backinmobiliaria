8bce2c131a4f182cfd18a9e92cc961f5593e24f9
JJRoma <jj_roma@yahoo.ccom.ar>
2022-06-13 20:06:01 -0300

    modifica user

--- src/main/java/com/proinmob/inmobiliaria/controller/Controller.java
+++ src/main/java/com/proinmob/inmobiliaria/controller/Controller.java
@@ -1,6 +1,7 @@
 
 package com.proinmob.inmobiliaria.controller;
 
+import com.proinmob.inmobiliaria.model.Usuarios;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import java.util.List;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,34 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     
 //------------------------------------- USUARIO ------------------------------------
-    @PostMapping("/user/new")
-    public void agregaUser(@RequestBody Users user ){
-        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
-        String hash = argon2.hash(1, 1024, 1, user.getPassword());
-        user.setPassword(hash);
+   @PostMapping("/user/new")
+    public void agregaUser(@RequestBody Usuarios user ){
+        user.setPassword(encripta(user.getPassword()));
         userServ.crearUser(user);
     }
     @GetMapping("/user/ver")
     @ResponseBody    
-    public List<Users> verUser(){
-        return userServ.verUsers();
+    public List<Usuarios> verUser(){
+        return userServ.verUsuario();
     }
     @DeleteMapping ("/user/delete/{id}")
     public void borrarUser ( @PathVariable Long id){
-        userServ.borrarUser(id);
+        userServ.borraUser(id);
     }
-    @PostMapping("/user/update")
-    public void acualizaUser(@RequestBody Users user ){
-        userServ.atualizarUser(user);
+    @PutMapping("/user/update")
+    public void acualizaUser(@RequestBody Usuarios user ){
+        user.setPassword(encripta(user.getPassword()));
+        user.setNuevo(false);
+        userServ.actualizaUser(user);
     }   
     @PostMapping("/user/ok")
     @ResponseBody    
-    public boolean verUser(@RequestBody Users user){
+    public boolean verUser(@RequestBody Usuarios user){
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
         
-        List<Users> usuarios =  userServ.verUsers();
+        List<Usuarios> usuarios =  userServ.verUsuario();
         
-        for( Users x:usuarios){
+        for( Usuarios x:usuarios){
             if((argon2.verify(x.getPassword(), user.getPassword()))&&(x.getUsuario().equals(user.getUsuario())) ){
                 x.setValido(true);
                 return true;
@@ -52,7 +54,11 @@ public class Controller {
         }
         return false;
     }
-
-//------------------------------------------------------------------------- 
+    private String encripta(String pass){
+         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+         String hash = argon2.hash(1, 1024, 1, pass);
+        return hash;
+    }
+ //----------------------------------------------------------------------------------  
     
 }
